42704c52f7b774ae2ae70b5c097e8bfd4f6b0bec
aaron <sicp618@gmail.com>
2023-03-02 09:08:26 +0800

    add readme

--- raffle/tasks/block-number.js
+++ raffle/tasks/block-number.js
@@ -2,7 +2,7 @@ const { task } = require("hardhat/config")
 
 task("block-number", "Prints the current block number").setAction(
     async (taskArgs, hre) => {
-        const bn = await hre.ethers.provider.getBlockNumber()
+        const bn = await hre.ethers.getDefaultProvider().getBlockNumber()
         console.info(`cure ${bn}`)
     }
 )
---
+++ readme.md
@@ -0,0 +1,12 @@
+# 区块链 Demo 合集
+## 演示
+goerli 测试网 [sicp618.com](https://sicp618.com)
+
+## 安装环境
+推荐先安装 `npm install --global hardhat-shorthand`，`raffle` 目录下的 `.env.example` 复制为 `.env` 并替换为自己的账号。
+
+## 后端本地部署
+`cd` 到 `raffle` 目录，执行 `hh node`。
+
+## 前端本地部署
+`cd` 到 `frontend` 目录，执行 `yarn install && yarn run dev`。
