876971d7393f2e49a535070be8c9006a6e1432fe
sicp618 <cc191954@gmail.com>
2023-05-17 18:31:25 +0800

    lottery support sepolia

--- raffle/hardhat.config.js
+++ raffle/hardhat.config.js
@@ -1,51 +1,60 @@
 require("@nomicfoundation/hardhat-toolbox");
-require("hardhat-gas-reporter")
-require("solidity-coverage")
-require("./tasks/block-number")
-require("hardhat-deploy")
-require("@nomiclabs/hardhat-etherscan")
-require("dotenv").config()
+require("hardhat-gas-reporter");
+require("solidity-coverage");
+require("./tasks/block-number");
+require("hardhat-deploy");
+require("@nomiclabs/hardhat-etherscan");
+require("dotenv").config();
 
-const GOERLI_API_KEY = process.env.GOERLI_API_KEY
-const GOERLI_PRIVATE_KEY = process.env.GOERLI_PRIVATE_KEY
-const COIN_MARKET_CAP = process.env.COIN_MARKET_CAP
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+const GOERLI_API_KEY = process.env.GOERLI_API_KEY;
+const COIN_MARKET_CAP = process.env.COIN_MARKET_CAP;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+const SEPOLIA_API_KEY = process.env.SEPOLIA_API_KEY;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
-  defaultNetwork: "hardhat",
-  networks: {
-    hardhat: {
-      chainId: 31337,
+    solidity: "0.8.17",
+    defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {
+            chainId: 31337,
+        },
+        goerli: {
+            url: `https://eth-goerli.g.alchemy.com/v2/${GOERLI_API_KEY}`,
+            accounts: [PRIVATE_KEY],
+            chainId: 5,
+        },
+        sepolia: {
+            url: `https://eth-sepolia.g.alchemy.com/v2/${SEPOLIA_API_KEY}`,
+            accounts: [PRIVATE_KEY],
+            chainId: 11155111,
+        },
+        localhost: {
+            url: "http://localhost:8545",
+            chainId: 1337,
+        },
     },
-    goerli: {
-      url: `https://eth-goerli.g.alchemy.com/v2/${GOERLI_API_KEY}`,
-      accounts: [ GOERLI_PRIVATE_KEY ],
-      chainId: 5,
+    etherscan: {
+        apiKey: {
+            sepolia: ETHERSCAN_API_KEY,
+            goerli: ETHERSCAN_API_KEY,
+        },
     },
-    localhost: {
-      url: "http://localhost:8545",
-      chainId: 1337
-    }
-  },
-  etherscan: {
-    apiKey: ETHERSCAN_API_KEY,
-  },
-  gasReporter: {
-    enabled: false,
-    outputFile: "gas-report.txt",
-    noColors: true,
-    currency: "USD",
-    coinmarketcap: COIN_MARKET_CAP
-  },
-  namedAccounts: {
-    deployer: {
-      default: 0,
-      1: 0
+    gasReporter: {
+        enabled: false,
+        outputFile: "gas-report.txt",
+        noColors: true,
+        currency: "USD",
+        coinmarketcap: COIN_MARKET_CAP,
     },
-    user: {
-      default: 1
-    }
-  }
+    namedAccounts: {
+        deployer: {
+            default: 0,
+            1: 0,
+        },
+        user: {
+            default: 1,
+        },
+    },
 };
--- raffle/helper-hardhat-config.js
+++ raffle/helper-hardhat-config.js
@@ -18,7 +18,17 @@ const networkConfig = {
         callbackGasLimit: "500000",
         interval: "30",
         numberOfPeople: "2",
-    }
+    },
+    11155111: {
+        name: "sepolia",
+        vrfCoordinatorV2: "0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625",
+        price: ethers.utils.parseEther("0.01"),
+        gasLane: "0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c",
+        subId: "2026",
+        callbackGasLimit: "500000",
+        interval: "30",
+        numberOfPeople: "2",
+    },
 }
 
 const developmentChains = ["hardhat", "localhost"]
